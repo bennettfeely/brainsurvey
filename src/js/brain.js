9ba22923d12e17bec81a8cf4afb46fb235bf717b
Bennett Feely <bfeelyweb@gmail.com>
2018-11-09 13:31:42 -0500

    New brain model 02

--- src/js/brain.js
+++ src/js/brain.js
@@ -2,21 +2,23 @@ if (WEBGL.isWebGLAvailable() === false) {
 	document.body.appendChild(WEBGL.getWebGLErrorMessage());
 }
 
-var controls, camera, scene, renderer, light;
+var mouse = new THREE.Vector2(),
+	INTERSECTED;
+var camera, controls, light, raycaster, renderer, scene;
 var settings = {
 	orbit: true,
 	orbit_speed: 4,
-	axes: false,
+	square_grid: true,
 	polar_grid: false,
-	square_grid: false,
-
+	axes: true,
 	pan: false,
 	zoom: false,
-
-	explode_regions: 0.8,
+	explode: 0,
+	model_path: "models/Brain_02/Geometry/Brain_02.gltf",
 
 	// Shouldn't need this eventually
-	y_offset: -12
+	x_offset: -1.5,
+	y_offset: -11.5
 };
 
 init();
@@ -28,12 +30,7 @@ function init() {
 	var canvasWidth = brain_wrapper.offsetWidth;
 	var canvasHeight = brain_wrapper.offsetHeight;
 
-	camera = new THREE.PerspectiveCamera(
-		50,
-		canvasWidth / canvasHeight,
-		0.1,
-		1000
-	);
+	camera = new THREE.PerspectiveCamera(50, canvasWidth / canvasHeight, 0.1, 1000);
 	camera.position.set(0, 10, 30);
 
 	controls = new THREE.OrbitControls(camera, brain_wrapper);
@@ -58,6 +55,9 @@ function init() {
 	// Set the scene
 	scene = new THREE.Scene();
 
+	// Setup raycaster for click detection
+	// raycaster = new THREE.Raycaster();
+
 	// Lighting
 	var light = new THREE.HemisphereLight(0xff9999, 0.5);
 	scene.add(light);
@@ -68,42 +68,103 @@ function init() {
 	// Model
 	var loader = new THREE.GLTFLoader();
 	loader.load(
-		"models/Brain_01/Geometry/Brain_01.gltf",
+		"models/Brain_02/Geometry/Brain_02.gltf",
 		function(gltf) {
+			i = 0;
 			gltf.scene.traverse(function(child) {
 				if (child.isMesh) {
+					console.log(i++);
+					// Create separate material instance
+					child.material = child.material.clone();
+
+					// D3 color scales
+					// https://github.com/d3/d3-scale-chromatic
+
+					var colors = [
+						"#1f77b4",
+						"#ff7f0e",
+						"#2ca02c",
+						"#d62728",
+						"#9467bd",
+						"#8c564b",
+						"#e377c2",
+						"#7f7f7f",
+						"#bcbd22",
+						"#17becf"
+					];
+
+					var colors = [
+						"tomato",
+						"mediumseagreen",
+						"orange",
+						"dodgerblue",
+						"orchid",
+						"lightgray",
+						"darkturquoise",
+						"palevioletred",
+						"khaki",
+						"limegreen",
+						"coral",
+						"slateblue",
+						"indianred",
+						"gray",
+						"plum",
+						"olivedrab",
+						"lightgreen",
+						"lightsalmon",
+						"gold",
+						"peru",
+						"royalblue",
+						"sandybrown",
+						"lightseagreen",
+						"blueviolet",
+						"tomato",
+						"goldenrod",
+						"limegreen"
+					];
+
+					child.material.color.set(colors[i]);
+
+					child.material.color.r = Math.random();
+					child.material.color.g = Math.random();
+					child.material.color.b = Math.random();
+
+					// child.scale.x = Math.random() * 4;
+					// child.scale.y = Math.random() * 4;
+					// child.scale.z = Math.random() * 4;
+
+					console.log(child);
+
 					// Explode brain regions
-					if (settings.explode_regions > 0) {
+					if (settings.explode > 0) {
 						child.geometry.computeBoundingSphere();
 
 						var x = child.geometry.boundingSphere.center.x;
 						var y = child.geometry.boundingSphere.center.y;
 						var z = child.geometry.boundingSphere.center.z;
 
-						var explode = 0.5;
-
 						child.position.set(
-							x * explode,
-							y * explode,
-							z * explode
+							x * settings.explode,
+							y * settings.explode,
+							z * settings.explode
 						);
 					}
 				}
 			});
 
-			gltf.scene.position.set(0, settings.y_offset, 0);
+			gltf.scene.position.set(
+				settings.x_offset,
+				settings.y_offset * (1 + settings.explode),
+				0
+			);
 			scene.add(gltf.scene);
+
 			animate();
 		},
 		function(xhr) {
 			// Update the loading indicator text
 			var pct = (xhr.loaded / xhr.total) * 100;
-
-			console.log("Loading: " + pct + "%");
-
-			if (pct < 100) {
-				loading_status.style.width = pct + "%";
-			}
+			loading_status.style.width = pct + "%";
 		},
 		function(error) {
 			console.log("An error happened");
@@ -118,8 +179,11 @@ function init() {
 
 	renderer.setSize(canvasWidth, canvasHeight);
 	renderer.gammaOutput = true;
+
+	// Add canvas to page
 	brain_wrapper.appendChild(renderer.domElement);
 
+	// Detect window resizing and resize canvas
 	window.addEventListener("resize", onWindowResize, false);
 }
 
